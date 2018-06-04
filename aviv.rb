b285007cbdef10993993618c4cf52349ea7d50be
Marc Poris <marc.poris@gmail.com>
2018-06-04 11:15:25 -0700

    Cleanup.

--- aviv.rb
+++ aviv.rb
@@ -1,28 +1,32 @@
+# Usage:
+#
+# ruby aviv.rb 5 "+"
+
 # Print a triangle
-def triangle(num_rows, what_to_print)
+def triangle(num_rows, word)
   1.upto num_rows do |row|
     num_spaces = num_rows - row
-    1.upto(num_spaces) { print " " * what_to_print.length }
+    1.upto(num_spaces) { print " " * word.length }
     num_stars = 2 * row - 1
-    1.upto(num_stars) { print what_to_print }
+    1.upto(num_stars) { print word }
     puts
   end
 end
 
 # Print upside down triangle (biangle according to Aviv)
-def biangle(num_rows, num_skip, what_to_print)
+def biangle(num_rows, num_skip, word)
   (num_rows - num_skip).downto 1 do |row|
     num_spaces = num_rows - row
-    num_spaces.times { print " " * what_to_print.length }
+    num_spaces.times { print " " * word.length }
     num_stars = 2 * row - 1
-    num_stars.times { print what_to_print }
+    num_stars.times { print word }
     puts
   end
 end
 
-def diamond(num_rows_in_top, what_to_print)
-  triangle num_rows_in_top, what_to_print
-  biangle num_rows_in_top, 1, what_to_print
+def diamond(num_rows_in_top, word)
+  triangle num_rows_in_top, word
+  biangle num_rows_in_top, 1, word
 end
 
 diamond ARGV[0].to_i, ARGV[1]
